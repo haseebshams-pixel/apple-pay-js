30847ea6450f55701b3929903696fc157f626cd5
haseebshams <haseebshams.cp@gmail.com>
2022-07-29 13:21:41 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
         let obj = { URL: event.validationURL };
         // Call your own server to request a new merchant session.
         axios
-          .post("http://localhost:8000/validateSession", obj)
+          .post("https://0460-202-163-76-70.in.ngrok.io/validateSession", obj)
           .then(function (response) {
             console.log(response);
             event.complete(response.data);
